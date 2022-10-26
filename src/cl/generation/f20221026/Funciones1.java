54b98cd9a1cb0054aa359cbd17db082f6c4b9c5c
Israel Alexis Palma Quezada <ipalm001@gmail.com>
2022-10-26 10:03:15 -0300

    Java basico: metodos, llamados y retornos con parametros

--- src/cl/generation/f20221026/Funciones1.java
+++ src/cl/generation/f20221026/Funciones1.java
@@ -1,4 +1,4 @@
-package cl.generation.f20221025;
+package cl.generation.f20221026;
 
 import java.util.Arrays;
 
@@ -16,12 +16,15 @@ public class Funciones1 {
 		String[] arreglito = {"a","e","i","o","u"};
 		metodo5(arreglito);
 		//metodos con llamados
+		//retorno1();//no hace nada con el valor retornado
 		//System.out.println(retorno1());
 		Integer valorRetornado = retorno1();
 		System.out.println(valorRetornado);
 		System.out.println(valorRetornado/7);//35/7=5
 		System.out.println(valorRetornado/5);//35/5=7
 		
+		String nombreCompleto = obtenerNombreCompleto("Quezada", "Israel", "Palma");
+		System.out.println("el nombre completo es: "+nombreCompleto);
 	}
 	
 //definicion o estructura de un metodo
@@ -29,7 +32,6 @@ public class Funciones1 {
 	public static void nombreMetodo() {
 		System.out.println("Estoy dentro del metodos inicial...");
 	}
-	
 	public static void metodo2(String palabra) {
 		System.out.println("Soy "+palabra);
 	}
@@ -44,10 +46,20 @@ public class Funciones1 {
 		System.out.println("Soy el arreglo "+Arrays.toString(arreglito));
 	}
 	//funciones con retorno
-	
 	public static Integer retorno1() {
 		Integer totalAlumnos= 35;
 		return totalAlumnos;
+	}
+	/**
+	 * Metodo que une todos los datos
+	 * @param apeMaterno
+	 * @param nombre
+	 * @param apePaterno
+	 * @return nombreCompleto
+	 */
+	public static String obtenerNombreCompleto(String apeMaterno,String nombre,String apePaterno) {
+		String nombreCompleto = nombre+" "+apePaterno+" "+apeMaterno;
+		return nombreCompleto;//retornamos el contenido de la variable
 	}
 	
 }
